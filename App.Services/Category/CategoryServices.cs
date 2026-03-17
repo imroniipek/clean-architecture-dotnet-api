0ac01a59edbd28e432e5c0357eeae9750e5179e9
imroniipek <roniipek8@gmail.com>
2026-03-17 20:26:44 +0300

    Cache Yapısı eklendi Category services kısmı cache yapısı kullanarak apileri yanıtlıyor

--- App.Services/Category/CategoryServices.cs
+++ App.Services/Category/CategoryServices.cs
@@ -1,4 +1,5 @@
 ﻿using System.Net;
+using App.Repository.Cache;
 using App.Repository.Repo.CategoryRepo;
 using App.Repository.UnitOfWork;
 using App.Services.Category.Create;
@@ -6,17 +7,24 @@ using App.Services.Category.Update;
 using App.Services.Dto;
 using App.Services.Exceptions;
 using Microsoft.EntityFrameworkCore;
+
 namespace App.Services.Category;
 
 public class CategoryService : ICategoryService
 {
     private readonly ICategoryRepository _categoryRepository;
     private readonly IUnitOfWork _unitOfWork;
+    private readonly ICacheService _cacheService;
 
-    public CategoryService(ICategoryRepository categoryRepository, IUnitOfWork unitOfWork)
+    private static readonly string CategoriesCacheKey = "categories-all";
+    private static readonly string CategoriesWithProductsCacheKey = "categories-with--products";
+    private static string CategoryWithProductsByIdCacheKey(int id) => $"categories:with-products:{id}";
+
+    public CategoryService(ICategoryRepository categoryRepository, IUnitOfWork unitOfWork, ICacheService cacheService)
     {
         _categoryRepository = categoryRepository;
         _unitOfWork = unitOfWork;
+        _cacheService = cacheService;
     }
 
     public async Task<ServiceResult.ServiceResult<int>> CreateCategoryAsync(CreateCategoryRequest request)
@@ -39,7 +47,8 @@ public class CategoryService : ICategoryService
 
         await _categoryRepository.AddAsync(category);
         await _unitOfWork.SaveAllChangesInDbAsync();
-
+        await _cacheService.RemoveAsync(CategoriesCacheKey);
+        await _cacheService.RemoveAsync(CategoriesWithProductsCacheKey);
         return ServiceResult.ServiceResult<int>.SuccessForCreated(category.Id, $"/api/categories/{category.Id}");
     }
 
@@ -49,7 +58,7 @@ public class CategoryService : ICategoryService
 
         if (category is null)
         {
-            throw new NotFoundException("Category",request.Id);
+            throw new NotFoundException("Category", request.Id);
         }
 
         var normalizedName = request.Name.Trim().ToLower();
@@ -60,14 +69,19 @@ public class CategoryService : ICategoryService
 
         if (isNameUsedByAnotherCategory)
         {
-            return ServiceResult.ServiceResult.Failed(HttpStatusCode.BadRequest,new[] { "Bu kategori adı başka bir kategoride zaten kullanılıyor." });
+            return ServiceResult.ServiceResult.Failed(
+                HttpStatusCode.BadRequest,
+                new[] { "Bu kategori adı başka bir kategoride zaten kullanılıyor." });
         }
 
         category.Name = request.Name.Trim();
 
         _categoryRepository.Update(category);
-
         await _unitOfWork.SaveAllChangesInDbAsync();
+
+        await _cacheService.RemoveAsync(CategoriesCacheKey);
+        await _cacheService.RemoveAsync(CategoriesWithProductsCacheKey);
+        await _cacheService.RemoveAsync(CategoryWithProductsByIdCacheKey(request.Id));
 
         return ServiceResult.ServiceResult.Success(HttpStatusCode.NoContent);
     }
@@ -78,33 +92,50 @@ public class CategoryService : ICategoryService
 
         if (category is null)
         {
-           throw new NotFoundException("Kategori Bulunamadı",id);
+            throw new NotFoundException("Kategori Bulunamadı", id);
         }
 
         _categoryRepository.Delete(category);
         await _unitOfWork.SaveAllChangesInDbAsync();
+
+        await _cacheService.RemoveAsync(CategoriesCacheKey);
+        await _cacheService.RemoveAsync(CategoriesWithProductsCacheKey);
+        await _cacheService.RemoveAsync(CategoryWithProductsByIdCacheKey(id));
 
         return ServiceResult.ServiceResult.Success(HttpStatusCode.NoContent);
     }
 
     public async Task<ServiceResult.ServiceResult<List<CategoryDto>>> GetAllCategoriesAsync()
     {
+        var cachedCategories = await _cacheService.GetAsync<List<CategoryDto>>(CategoriesCacheKey);
+
+        if (cachedCategories is not null)
+        {
+            return ServiceResult.ServiceResult<List<CategoryDto>>.Success(cachedCategories);
+        }
+
         var categories = await _categoryRepository
             .GetAll()
             .AsNoTracking()
             .Select(x => new CategoryDto(x.Id, x.Name))
             .ToListAsync();
 
-        if (categories.Count == 0)
-        {
-            //throw new NotFoundException("Categoriler Listesi Bulunamadı");
-        }
+        await _cacheService.AddAsync(CategoriesCacheKey, categories, TimeSpan.FromMinutes(5));
 
         return ServiceResult.ServiceResult<List<CategoryDto>>.Success(categories);
     }
 
     public async Task<ServiceResult.ServiceResult<CategoryWithProductsDto>> GetCategoryWithProductsByIdAsync(int id)
     {
+        var cacheKey = CategoryWithProductsByIdCacheKey(id);
+
+        var cachedCategory = await _cacheService.GetAsync<CategoryWithProductsDto>(cacheKey);
+
+        if (cachedCategory is not null)
+        {
+            return ServiceResult.ServiceResult<CategoryWithProductsDto>.Success(cachedCategory);
+        }
+
         var category = await _categoryRepository.GetCategoryWithProductsAsync(id);
 
         if (category is null)
@@ -112,13 +143,26 @@ public class CategoryService : ICategoryService
             return ServiceResult.ServiceResult<CategoryWithProductsDto>.Failed(new[] { "Kategori bulunamadı." });
         }
 
-        var categoryDto = new CategoryWithProductsDto(category.Id, category.Name, category.ProductList.Select(p => new ProductDto(p.Id, p.Name, p.Price, p.Count, p.CategoryId)).ToList());
+        var categoryDto = new CategoryWithProductsDto(
+            category.Id,
+            category.Name,
+            category.ProductList.Select(p => new ProductDto(p.Id, p.Name, p.Price, p.Count, p.CategoryId)).ToList()
+        );
+
+        await _cacheService.AddAsync(cacheKey, categoryDto, TimeSpan.FromMinutes(10));
 
         return ServiceResult.ServiceResult<CategoryWithProductsDto>.Success(categoryDto);
     }
 
     public async Task<ServiceResult.ServiceResult<List<CategoryWithProductsDto>>> GetAllCategoriesWithProductsAsync()
     {
+        var cachedCategories = await _cacheService.GetAsync<List<CategoryWithProductsDto>>(CategoriesWithProductsCacheKey);
+
+        if (cachedCategories is not null)
+        {
+            return ServiceResult.ServiceResult<List<CategoryWithProductsDto>>.Success(cachedCategories);
+        }
+
         var categories = await _categoryRepository
             .GetAllCategoriesWithProducts()
             .AsNoTracking()
@@ -127,8 +171,11 @@ public class CategoryService : ICategoryService
         var result = categories.Select(category => new CategoryWithProductsDto(
             category.Id,
             category.Name,
-            category.ProductList.Select(p => new ProductDto(p.Id, p.Name, p.Price, p.Count, p.CategoryId)).ToList())).ToList();
+            category.ProductList.Select(p => new ProductDto(p.Id, p.Name, p.Price, p.Count, p.CategoryId)).ToList()
+        )).ToList();
+
+        await _cacheService.AddAsync(CategoriesWithProductsCacheKey, result, TimeSpan.FromMinutes(10));
+
         return ServiceResult.ServiceResult<List<CategoryWithProductsDto>>.Success(result);
     }
-
 }
